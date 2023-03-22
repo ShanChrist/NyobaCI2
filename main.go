e2f661fe5d42baccd80c504a67cf33848b04f07e
ShanChrist <shnchrst@gmail.com>
2023-03-22 15:09:26 +0700

    fix if statment

--- main.go
+++ main.go
@@ -6,8 +6,7 @@ import (
 )
 
 func main() {
-	var x = 0
-	if x = 1; x == 1 { // This line has a mistake, should use the := operator instead of = to declare and assign a new variable.
+	if x := 1; x == 1 {
 		fmt.Println("x is equal to 1")
 	}
 
