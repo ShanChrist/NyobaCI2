7b7f21d877cba82f79ebb6811c5d426c9f9636a6
ShanChrist <shnchrst@gmail.com>
2023-03-22 14:55:18 +0700

    add calculation

--- main.go
+++ main.go
@@ -1,10 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+)
 
 func main() {
 	var x = 0
 	if x = 1; x == 1 { // This line has a mistake, should use the := operator instead of = to declare and assign a new variable.
 		fmt.Println("x is equal to 1")
 	}
+
+	a := 10
+	b := 10
+	log.Println(a + b)
 }
