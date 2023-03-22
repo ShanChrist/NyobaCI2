a961e7a004efae73bb2f2df7e4d8904fe4f27acb
ShanChrist <shnchrst@gmail.com>
2023-03-22 17:38:52 +0700

    nyoba-ci

--- main.go
+++ main.go
@@ -1,10 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+)
 
 func main() {
-	var x = 0
-	if x = 1; x == 1 { // This line has a mistake, should use the := operator instead of = to declare and assign a new variable.
+	if x := 1; x == 1 {
 		fmt.Println("x is equal to 1")
 	}
+
+	a := 10
+	b := 10
+	log.Println(a + b)
 }
